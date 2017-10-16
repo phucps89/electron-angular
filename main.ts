275ef47c01e63fa43e0010ffc6a7310e860ce4da
phuctran <phuc.ps.89@gmail.com>
2017-10-16 21:04:33 +0700

    init

--- main.ts
+++ main.ts
@@ -19,8 +19,9 @@ function createWindow() {
   win = new BrowserWindow({
     x: 0,
     y: 0,
-    width: size.width,
-    height: size.height
+    width: 960,
+    height: 700,
+    resizable: false
   });
 
   // and load the index.html of the app.
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title = `App works !`;
+  title = `App works 456!`;
 
-  constructor() { }
+  constructor() {
+
+    $(document).ready(function(){
+      var mySlider = $("input.slider").slider();
+    })
+
+  }
 
   ngOnInit() {
   }
